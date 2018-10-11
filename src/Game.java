160de68ecdbbe36f5e9447af70f0d526d4a29ff0
BT_1N3_33 <ljaoutakas8934@bths.edu>
2018-10-11 14:48:03 -0400

    Obj1

--- src/Game.java
+++ src/Game.java
@@ -1,38 +1,73 @@
 import java.util.Scanner;
 public class Game
 {
-    public static void main(String args[])
+
+    public static void main(String [] args)
     {
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("Which game do you want to play?");
+        System.out.println("Press ''1'' to make the computer guess, ''2'' if you want to guess.");
+        if (input1.nextInt() == 1)
+        {
+            aigame();
+        }
+        if (input1.nextInt() == 2)
+        {
+            guessgame();
+        }
+    }
+    public static void aigame()
+    {
+        Scanner input3 = new Scanner (System.in);
+        System.out.println("Think of a number from 1 to 1000");
+        int lowerlim = 0;
+        int upperlim = 1000;
+        int guess = 500;
+        int numguess = 1;
+        String useri = "";
+        while (true)
+        {
+            System.out.println("Is your number greater than, (type: 'g') less than, (type: 'l') or equal to ('type: e') the number " + guess + "?");
+            //make a variable and set it to next line, and check that.
+            useri = input3.nextLine();
+            if (useri.equals("g"))
+            {
+                lowerlim = guess;
+            }
+            if (useri.equals("l"))
+            {
+                upperlim = guess;
+            }
+            if (useri.equals("e"))
+            {
+                break;
+            }
+            guess = (int)(Math.random()*(upperlim-lowerlim)+lowerlim);
+            numguess++;
+        }
+        System.out.println("Great!");
+
+    }
+    public static void guessgame()
+    {
+        Scanner input2 = new Scanner(System.in);
         int num1 = (int) (Math.random() * 100 + 1);
-        Scanner input = new Scanner(System.in);
         int guess = 101;
+        int numguess = 1;
+        String guesses = "";
         System.out.println("Guess the number:");
         System.out.println("it is "+num1);
         while (guess != num1)
         {
-            guess = input.nextInt();
+            guess = input2.nextInt();
             if (guess != num1)
             {
+                numguess++;
                 System.out.println("Guess again");
             }
+            guesses = guesses + " " + Integer.toString(guess);
         }
-        System.out.println("You got it!");
-    }
-    public static int ai(String value)
-    {
-        int a = 50;
-
-        while (int)
-        {
-            if (value.equals("The number is lower than you guessed"))
-            {
-
-            }
-            else
-            {
-
-            }
-        }
-        return a;
+        System.out.println("You got it in " + numguess + " tries!");
+        System.out.println("These were your guesses: " + guesses );
     }
 }
